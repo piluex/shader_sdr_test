a9f6df1b8017bd64dfcc6de37cf32bae16e210b1
Esteban Torre <esteban.torre@gmail.com>
2018-09-23 00:51:57 -0300

    sucky gcc doesn't work with Os

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-c++ -fpermissive convenience.c sdr_demo.c -I /usr/include -lSDL2 -lGL -lrtlsdr -lGLEW -o demo
+c++  -fpermissive convenience.c sdr_demo.c -I /usr/include -lSDL2 -lGL -lrtlsdr -lGLEW -o demo
+
 ./demo
